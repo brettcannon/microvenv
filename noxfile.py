d12f844d7be224327df6498bba22e02382e65747
Brett Cannon <brett@python.org>
2023-03-07 01:04:28 +0000

    Set up CI Closes #6

---
+++ noxfile.py
@@ -0,0 +1,14 @@
+import nox  # type: ignore
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11", "3.12", "pypy3.9"])
+def test(session):
+    session.install("pytest")  # XXX: get from pyproject.toml
+    session.run("pytest")
+
+
+@nox.session
+def lint(session):
+    session.install("black", "ruff")  # XXX: get from pyproject.toml
+    session.run("ruff", "check", ".")
+    session.run("black", "--check", ".")
