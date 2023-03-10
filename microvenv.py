19fa5a8aa0a1a94a343097087b8dc4047527c127
Brett Cannon <brett@python.org>
2023-03-10 22:38:05 +0000

    Add a public API via the `create()` function Closes #18

--- microvenv.py
+++ microvenv.py
@@ -3,37 +3,40 @@ import pathlib
 import sys
 import sysconfig
 
-PYVENVCFG_TEMPLATE = f"""home = {{base_executable_parent}}
+_BASE_EXECUTABLE = pathlib.Path(getattr(sys, "_base_executable", sys.executable))
+
+_PYVENVCFG_TEMPLATE = f"""home = {_BASE_EXECUTABLE.parent}
 include-system-site-packages = false
-version = {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}
-executable = {{resolved_base_executable}}
+version = {'.'.join(map(str, sys.version_info[:3]))}
+executable = {_BASE_EXECUTABLE.resolve()}
 command = {{command}}
 """
 
 
-def _sysconfig_path(name, venv_dir):
+def _sysconfig_path(name, env_dir):
     variables = {
-        "base": venv_dir,
-        "platbase": venv_dir,
-        "installed_base": venv_dir,
-        "installed_platbase": venv_dir,
+        "base": env_dir,
+        "platbase": env_dir,
+        "installed_base": env_dir,
+        "installed_platbase": env_dir,
     }
 
     return pathlib.Path(sysconfig.get_path(name, "venv", variables))
 
 
-def create(venv_dir):
-    base_executable = pathlib.Path(getattr(sys, "_base_executable", sys.executable))
-
+# Analogous to `venv.create()`.
+def create(env_dir=".venv"):
+    """Create a minimal virtual environment."""
+    env_path = pathlib.Path(env_dir)
     try:
-        scripts_dir = _sysconfig_path("scripts", venv_dir)
-        include_dir = _sysconfig_path("include", venv_dir)
-        purelib_dir = _sysconfig_path("purelib", venv_dir)
+        scripts_dir = _sysconfig_path("scripts", env_path)
+        include_dir = _sysconfig_path("include", env_path)
+        purelib_dir = _sysconfig_path("purelib", env_path)
     except KeyError:
-        scripts_dir = venv_dir / "bin"
-        include_dir = venv_dir / "include"
+        scripts_dir = env_path / "bin"
+        include_dir = env_path / "include"
         purelib_dir = (
-            venv_dir
+            env_path
             / "lib"
             / f"python{sys.version_info.major}.{sys.version_info.minor}"
             / "site-packages"
@@ -42,38 +45,34 @@ def create(venv_dir):
         dir.mkdir(parents=True)
 
     if sys.maxsize > 2**32 and os.name == "posix" and sys.platform != "darwin":
-        (venv_dir / "lib64").symlink_to("lib", target_is_directory=True)
+        (env_path / "lib64").symlink_to("lib", target_is_directory=True)
 
     for executable_name in (
         "python",
         f"python{sys.version_info.major}",
         f"python{sys.version_info.major}.{sys.version_info.minor}",
     ):
-        (scripts_dir / executable_name).symlink_to(base_executable)
+        (scripts_dir / executable_name).symlink_to(_BASE_EXECUTABLE)
 
     try:
         module_path = pathlib.Path(__file__).resolve()
     except NameError:
         command = f"{sys.executable} -c '...'"
     else:
-        command = f"{sys.executable} {module_path} {venv_dir.resolve()}"
-    (venv_dir / "pyvenv.cfg").write_text(
-        PYVENVCFG_TEMPLATE.format(
-            base_executable_parent=base_executable.parent,
-            resolved_base_executable=base_executable.resolve(),
-            command=command,
-        ),
+        command = f"{sys.executable} {module_path} {env_path.resolve()}"
+    (env_path / "pyvenv.cfg").write_text(
+        _PYVENVCFG_TEMPLATE.format(command=command),
         encoding="utf-8",
     )
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 2:
-        print("Usage: microvenv.py [venv_dir='.venv']", file=sys.stderr)
+        print("Usage: microvenv.py [env_dir='.venv']", file=sys.stderr)
         sys.exit(1)
     try:
-        venv_dir = sys.argv[1]
+        env_dir = sys.argv[1]
     except IndexError:
-        venv_dir = ".venv"
+        env_dir = ".venv"
 
-    create(pathlib.Path(venv_dir))
+    create(env_dir)
