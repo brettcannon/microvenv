d1916883259641f519b42e9378504e448e72c30b
Brett Cannon <brett@python.org>
2023-09-17 13:28:50 -0700

    Add `IN_VIRTUAL_ENV` (#47)
    Closes #39

--- microvenv/__init__.py
+++ microvenv/__init__.py
@@ -1,1 +1,8 @@
+import sys
+
+# Exported as part of the public API.
 from ._create import create as create
+
+# https://docs.python.org/3/library/venv.html#how-venvs-work
+IN_VIRTUAL_ENV = sys.prefix != sys.base_prefix
+
