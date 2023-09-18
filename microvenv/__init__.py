f34f7b3d8254c275dc1bf4f1b014a9655edfd82e
Brett Cannon <brett@python.org>
2023-09-17 18:44:32 -0700

    Write comments and docs for what's planned

--- microvenv/__init__.py
+++ microvenv/__init__.py
@@ -1,3 +1,4 @@
+import os
 import pathlib
 import sys
 
@@ -32,3 +33,37 @@ def parse_config(env_dir):
                 key, _, value = line.partition("=")
                 config[key.strip().lower()] = value.strip()
     return config
+
+
+# There are three possible APIs for helping with virtual environment activation:
+# 1. Mutate the environment variable dictionary in-place;
+#    ``activate(os.environ.copy())``
+# 2. Return a mutated copy; ``activated_env_copy()``
+# 3. Calculate the environment variables to add; ``os.environ | activation()``
+#
+# Option 1 has the issue that if you forget to pass in a copy you will end up
+# mutating the running process' environment (in most cases).
+# Option 2 has the issue of needing an awkward name to communicate what exactly
+# is occurring.
+# Option 3 has the issue of not being able to delete environment variables
+# (i.e., PYTHONHOME).
+#
+# The decision was to go with option 3 with an exception if PYTHONHOME is set as
+# it's the most self-documenting for the common case of PYTHONHOME not already
+# being set.
+def activation(env_vars=os.environ):
+    """Returns a dict with env vars to activate the current virtual environment.
+
+    If the current interpreter is not from a virtual environment,
+    ActivationError is raised.
+
+    Since this API is designed to be additive to a environment variables dict
+    (i.e., ``os.environ | activation()``), key deletions are not supported. As
+    such, ActivationError is raised if PYTHONHOME is found to be set.
+
+    No environment variables are provided in relation to shell prompts.
+    """
+    # XXX in a virtual environment
+    # XXX PYTHONHOME
+    # XXX PATH
+    # XXX VIRTUAL_ENV
