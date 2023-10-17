ee3b599b2e15e0c409641ad4c8313c9a28f654d9
Brett Cannon <brett@python.org>
2023-10-17 12:43:29 -0700

    Prevent `__main__.py` from attempting to execute on Windows. (#57)
    Closes #56

--- microvenv/__main__.py
+++ microvenv/__main__.py
@@ -1,3 +1,9 @@
+import sys
+
+if sys.platform == "win32":
+    print("Microvenv's CLI is not supported on Windows", file=sys.stderr)
+    sys.exit(1)
+
 from ._create import main
 
 if __name__ == "__main__":
