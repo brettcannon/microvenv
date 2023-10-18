6db4349196eb153edd43a8da569d640a90e46ee8
Brett Cannon <brcan@microsoft.com>
2023-10-18 14:50:28 -0700

    Get mypy passing under Windows Closes #56

--- microvenv/_create.pyi
+++ microvenv/_create.pyi
@@ -1,11 +1,9 @@
 from os import PathLike
-import sys
 from typing import Iterable
 
 DEFAULT_ENV_DIR: str
 
-if sys.platform != "win32":
-    def create(
-        env_dir: str | PathLike[str] = ..., *, scm_ignore_files=Iterable[str]
-    ) -> None: ...
-    def main() -> None: ...
+def create(
+    env_dir: str | PathLike[str] = ..., *, scm_ignore_files=Iterable[str]
+) -> None: ...
+def main() -> None: ...
