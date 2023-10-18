7cdcf908438fd1e8b2370624ec80a9ee44a597ec
Brett Cannon <brett@python.org>
2023-10-18 14:55:03 -0700

    Get mypy passing under Windows (#58)
    Closes #56

--- microvenv/__main__.py
+++ microvenv/__main__.py
@@ -1,10 +1,10 @@
 import sys
 
-if sys.platform == "win32":
-    print("Microvenv's CLI is not supported on Windows", file=sys.stderr)
-    sys.exit(1)
-
 from ._create import main
 
 if __name__ == "__main__":
+    if sys.platform == "win32":
+        print("Microvenv's CLI is not supported on Windows", file=sys.stderr)
+        sys.exit(1)
+
     main()
--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,9 @@ def executable():
 @pytest.fixture(scope="session")
 def full_venv(tmp_path_factory):
     """Create a virtual environment via venv."""
+    symlinks = sys.platform != "win32"
     venv_path = tmp_path_factory.mktemp("venvs") / "full_venv"
-    venv.create(venv_path, symlinks=True, with_pip=False, system_site_packages=False)
+    venv.create(
+        venv_path, symlinks=symlinks, with_pip=False, system_site_packages=False
+    )
     return venv_path
